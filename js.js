d70bb137ce565920c967b9ddc8fd37074bc5eaa9
Vince1993 <v,huegen@icloud.com>
2026-02-11 09:14:36 +0100

    first_commit

---
+++ js.js
@@ -0,0 +1,49 @@
+$(function () {
+    const $grid = $('.grid_container');
+
+    $.each(data.images, function (_, img) {
+        const $item = $(`
+            <div class="image_container">
+                <img src="raw/${img.file}" alt="${img.title || ''}">
+                <h3>${img.title || ''}</h3>
+                <h3>${img.description || ''}</h3>
+            </div>`);
+        $grid.append($item);
+    });
+});
+  
+const data = {
+    images: [
+      { file: "IMG_20250506_105602_1.jpg", description: "Stadhagen", title: "Mary"},
+      { file: "IMG_20250506_105611_1.jpg", description: "Stadhagen", title: "Geisha 1"},
+      { file: "IMG_20260210_082431.jpg", description: "Glenorchy, New Zealand", title: "gebundene Blumen"},
+      { file: "IMG_20260210_082614.jpg", description: "Glenorchy, New Zealand", title: "Blumen" },
+      { file: "IMG_20260210_082712.jpg", description: "Glenorchy, New Zealand", title: "Caro"  },
+      { file: "IMG_20260210_082939.jpg", description: "Wunstorf", title: "Nedved" },
+      { file: "IMG_20260210_083443.jpg", description: "Wunstorf", title: "Ein Kolibri"},
+      { file: "IMG_20260210_083509.jpg", description: "Wanaka, New Zealand", title: "Taylor" },
+      { file: "IMG_20260210_083651.jpg", description: "Little River, New Zealand", title: "Love"},
+      { file: "IMG_20260210_083758.jpg", description: "Little River, New Zealand", title: "Die Lilie"  },
+      { file: "IMG_20260210_083841.jpg", description: "Little River, New Zealand", title: "Blumemstrauß"  },
+      { file: "IMG_20260210_084026.jpg", description: "Little River, New Zealand", title: "Fantasievogel"  },
+      { file: "IMG_20260210_084043.jpg", description: "Little River, New Zealand", title: "fliegender Kolibir"},
+      { file: "IMG_20260210_084120.jpg", description: "Little River, New Zealand", title: "Fischvogel"},
+      { file: "IMG_20260210_084543.jpg", description: "Wunstorf", title: "Abstrakt"},
+      { file: "IMG_20260210_084645.jpg", description: "Wunstorf", title: "lila Blumen"},
+      { file: "IMG_20260210_084741.jpg", description: "Wunstorf", title: "weiße Blüte"},
+      { file: "IMG_20260210_084847.jpg", description: "Wunstorf", title: "japanische Frau" },
+      { file: "IMG_20260210_084917.jpg", description: "Riverton, New Zealand", title: "Hektordelphin 1"},
+      { file: "IMG_20260210_084954.jpg", description: "Wunstorf", title: "Hektordelphin 2"},
+      { file: "IMG_20260210_085106.jpg", description: "Wunstorf", title: "Gelbaugen Pinguin"},
+      { file: "IMG_20260210_085153.jpg", description: "Wunstorf", title: "Möwe im Flug"},
+      { file: "IMG_20260210_085309.jpg", description: "Wunstorf", title: "Fischsymbol"},
+      { file: "IMG_20260210_085325.jpg", description: "Wunstorf", title: "Kererū"},
+      { file: "IMG_20260210_085340.jpg", description: "Wunstorf", title: "Abstrakt"},
+      { file: "IMG_20260210_085426.jpg", description: "Wunstorf", title: "schöner Kolibri"},
+      { file: "IMG_20260210_085547.jpg", description: "Wunstorf", title: "Weihnachtslilie"},
+      { file: "IMG_20260210_085707.jpg", description: "Wunstorf", title: "Geisha 2"},
+      { file: "IMG_20260210_085732.jpg", description: "Wunstorf", title: "Wal im Sprung" },
+      { file: "IMG_20260210_085850.jpg", description: "Little River, New Zealand", title: "Transparente Blume"},
+      { file: "IMG_20260210_085921.jpg", description: "Wunstorf", title: "Geisha 3"}
+    ]
+  };
---
+++ readme.txt
@@ -0,0 +1,4 @@
+https://unsplash.com/photos/a-painting-of-flowers-on-a-white-background-hRGjmrhXnag?utm_source=unsplash&utm_medium=referral&utm_content=creditShareLink
+
+
+Video by <a href="https://pixabay.com/users/engin_akyurt-3656355/?utm_source=link-attribution&utm_medium=referral&utm_campaign=video&utm_content=91074">Engin Akyurt</a> from <a href="https://pixabay.com//?utm_source=link-attribution&utm_medium=referral&utm_campaign=video&utm_content=91074">Pixabay</a>
